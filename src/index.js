2a3564ba28b0f1c2c4a44d8a94fdf588d1b80d39
Glitch (starter-react-template) <none>
2020-03-29 21:04:53 +0000

    🎏🐛 Checkpoint ./package.json:158517/1003 ./src/index.js old:158517/6156 ./src/index.js:158517/13305

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import InfiniteScroll from "react-infinite-scroll-component";
 import './index.css';
 
 class Body extends React.Component {
@@ -23,28 +24,29 @@ class Body extends React.Component {
                         </div>
                     </div>
                     <div id="content" className="col-md-9 col-lg-10 col-sm-8 col-xs-6">
-                        <div
-                            className="row"
-                            style={{ textAlign: "center", marginTop: "-25px", marginBottom: 10 }}
-                        >
-                        <div className="col-md-6 col-md-offset-3 col-sm-4 col-sm-offset-4 col-xs-12 col-xs-offset-0">
-                            <h2>Top Twitch Streams</h2>
+                        <div id="headerMain">
+                            <div className="row"
+                                style={{ textAlign: "center", marginTop: "-25px", marginBottom: 10 }}
+                            >
+                                <div className="col-md-6 col-md-offset-3 col-sm-4 col-sm-offset-4 col-xs-12 col-xs-offset-0">
+                                    <h2>Top Twitch Streams</h2>
+                                </div>
+                            </div>
+                            <div id ="search" className="row sticky" style={{"padding-bottom": "20px"}}>
+                                <div className="col-md-6 col-md-offset-3 col-sm-4 col-sm-offset-4 col-xs-12 col-xs-offset-0">
+                                    <input
+                                        type="text"
+                                        id="search"
+                                        className="form-control"
+                                        placeholder="type game or channel"
+                                        autofocus
+                                        oninput="search(this.value)"
+                                        onfocus="this.value=''"
+                                    />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6 col-md-offset-3 col-sm-4 col-sm-offset-4 col-xs-12 col-xs-offset-0">
-                            <input
-                                type="text"
-                                id="search"
-                                className="form-control"
-                                placeholder="type game or channel"
-                                autofocus
-                                oninput="search(this.value)"
-                                onfocus="this.value=''"
-                            />
-                        </div>
-                    </div>
-                    <div id="streams" className="row" />
+                        <div id="streams" className="row" />
                     </div>
                 </div>
             </div>
@@ -113,36 +115,84 @@ class Thumb extends React.Component{
         )
     }
 }
-var topStreams = 'https://api.twitch.tv/kraken/streams?limit=40&client_id=5umdahxg39d3df4xvs0vbmlmurd9xoc';
-function getJson(uri,callback){
-    var requestURL = new XMLHttpRequest();
-    requestURL.open("GET", uri || 'https://api.twitch.tv/kraken/streams?limit=40&client_id=5umdahxg39d3df4xvs0vbmlmurd9xoc');
-    requestURL.setRequestHeader("Client-ID", "kimne78kx3ncx6brgo4mv6wki5h1ko");
-    requestURL.overrideMimeType('application/vnd.twitchtv.v5+json');
-    requestURL.send();
-    requestURL.onreadystatechange = function() {
-        if (this.readyState !== 4) return;
 
-        // по окончании запроса доступны:
-        // status, statusText
-        // responseText, responseXML (при content-type: text/xml)
 
-        if (this.status !== 200) {
-            // обработать ошибку
-            alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
-            return;
-        }
-        var json = JSON.parse(this.responseText);
-        //console.log(json);
-        if (callback) {
-            callback(json);
-        }
-        
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      url: 'https://api.twitch.tv/kraken/streams?limit=40',
+      pageNo: 1,
+      displayCount: 5,
+      noMoreData: false
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+
+  fetchData() {
+    const { items, url, pageNo, noMoreData } = this.state;
+
+    if (noMoreData) {
+      console.log('no more data');
+      return;
     }
+    fetch(url, {
+      headers: {
+        'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
+        'Content-type': 'application/vnd.twitchtv.v5+json'
+      }
+    })
+      .then(res => res.json())
+      .then(
+          (result) => {
+              this.setState({
+              isLoaded: true,
+              items: [ ...items, ...result.streams ],
+              url: result._links.next,
+              pageNo: pageNo + 1
+              });
+          },
+          // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+          // чтобы не перехватывать исключения из ошибок в самих компонентах.
+          (error) => {
+              this.setState({
+              isLoaded: true,
+              error
+              });
+          }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+          <InfiniteScroll
+            dataLength={this.state.items.length}
+            next={this.fetchData}
+            hasMore={!this.state.noMoreData}
+            loader={<h4>Loading...</h4>}
+            style={{overflow: "hidden"}}
+          >
+            { items.map((item, index) => (
+              <Stream key={`item-${index}`} channel_name={item.channel.name} display_name={item.channel.display_name} game={item.channel.game} viewers={item.viewers} />
+            ))}
+          </InfiniteScroll>
+      );
+    }
+  }
 }
-getJson(topStreams, (json)=>{
-    //console.log(json);
-    let array = [];
-    for (var i=0; i<json.streams.length;i++) array.push(<Stream id={i} channel_name={json.streams[i].channel.name} display_name={json.streams[i].channel.display_name} game={json.streams[i].channel.game} viewers={json.streams[i].viewers} />);
-    ReactDOM.render(array, document.getElementById('streams'));
-});
+ReactDOM.render(<App />, document.getElementById('streams'));
